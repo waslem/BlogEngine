d8f6cead8dabf5113747452061255a454980109d
Jamie van Walsum <jvw@westnet.com.au>
2013-10-17 12:33:25 +0800

    front end work on boostrap3 template for admin area

---
+++ BlogEngine.Web/Helpers/CustomHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogEngine.Web.Helpers
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString Timeago(this HtmlHelper helper, DateTime dateTime)
+        {
+            return MvcHtmlString.Create(dateTime.ToRelativeDate());
+        }
+    }
+}
--- BlogEngine.Web/Helpers/DateFormatter.cs
+++ BlogEngine.Web/Helpers/DateFormatter.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Globalization;
+using System.Web.Mvc;
 
 namespace BlogEngine.Web.Helpers
 {
-    public class DateFormatter
+    public static class DateFormatter
     {
         public static string Format(DateTime date)
         {
@@ -16,6 +17,39 @@ namespace BlogEngine.Web.Helpers
                             + date.ToShortTimeString();
 
             return dateFormatted;
+        }
+
+        public static string FormatShort(DateTime dateTime)
+        {
+            string dateFormatted = dateTime.ToString("dd")
+                                    + "/"
+                                    + dateTime.ToString("MM")
+                                    + "/"
+                                    + dateTime.ToString("yyyy");
+
+            return dateFormatted;
+        }
+
+        public static string ToRelativeDate(this DateTime dateTime)
+        {
+            var timeSpan = DateTime.Now - dateTime;
+
+            if (timeSpan <= TimeSpan.FromSeconds(60))
+                return string.Format("{0} seconds ago", timeSpan.Seconds);
+
+            if (timeSpan <= TimeSpan.FromMinutes(60))
+                return timeSpan.Minutes > 1 ? String.Format("{0} minutes ago", timeSpan.Minutes) : "a minute ago";
+
+            if (timeSpan <= TimeSpan.FromHours(24))
+                return timeSpan.Hours > 1 ? String.Format("{0} hours ago", timeSpan.Hours) : "an hour ago";
+
+            if (timeSpan <= TimeSpan.FromDays(30))
+                return timeSpan.Days > 1 ? String.Format("{0} days ago", timeSpan.Days) : "a day ago";
+
+            if (timeSpan <= TimeSpan.FromDays(365))
+                return timeSpan.Days > 30 ? String.Format("{0} months ago", timeSpan.Days / 30) : "a month ago";
+
+            return timeSpan.Days > 365 ? String.Format("{0} years ago", timeSpan.Days / 365) : "a year ago";
         }
     }
 }
